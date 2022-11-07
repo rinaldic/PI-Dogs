57b50afb1d83dd1f1889b7143a637be7535719d4
Carlos R <rinaldic@gmail.com>
2022-11-07 19:16:49 -0300

    deploy4

--- api/src/db.js
+++ api/src/db.js
@@ -12,6 +12,12 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  pool: {     // This is to increse the acquire time and avoid SequelizeConnectionAcquireTimeoutError 
+    max: 30,
+    min: 0,
+    acquire: 60000,
+    idle: 5000
+   } ,
 });
 
 
